078d7461484fc9541b03d35ade44adabd1950f43
Gaétan Zwick <gaetan.zwick@gmail.com>
2020-04-10 17:12:13 +0200

    fmdksm

---
+++ cmake-build-debug/CMakeFiles/INF2_Labo4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/INF2_Labo4.dir/main.cpp.obj"
+  "INF2_Labo4.exe"
+  "INF2_Labo4.exe.manifest"
+  "INF2_Labo4.pdb"
+  "libINF2_Labo4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/INF2_Labo4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
